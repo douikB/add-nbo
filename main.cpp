fdb251c30a77455837e81f0c7bc2ec09cd05226b
13douik <13duoik@gmail.com>
2024-03-10 21:09:30 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,34 @@
 
 
 int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("2개의 파일이 필요합니다.\n");
+		return 1;
+	}
+
 	FILE* fp1, * fp2;
-	uint32_t a, b, c;
+	uint32_t a, b, c, fs1, fs2;
 
 	fp1 = fopen(argv[1], "rb");
 	fp2 = fopen(argv[2], "rb");
+
+	if (fp1 == NULL || fp2 == NULL){
+		printf("파일을 여는 것에 실패했습니다.\n");
+		return 1;
+	}
+
+	fseek(fp1, 0, SEEK_END);
+	fs1 = ftell(fp1);
+	fseek(fp1, 0, SEEK_SET);
+
+	fseek(fp2, 0, SEEK_END);
+	fs2 = ftell(fp2);
+	fseek(fp2, 0, SEEK_SET);
+
+	if (fs1 < 4 || fs2 < 4) {
+		printf("파일의 크기가 4byte보다 작아서 읽을 수 없습니다.\n");
+		return 1;
+	}	
 
 	fread(&a, 4, 1, fp1);
 	fread(&b, 4, 1, fp2);
